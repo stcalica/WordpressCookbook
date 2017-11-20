13bd05e8aefa27d24f092a60176bd6e432e867e0
stcalica <kyle@paper-street.local>
2017-11-19 18:51:42 -0800

    basic chef recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,27 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+package 'git'
+
+pacakge 'vim'
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  supports status: true, restart: true, reload: true
+  action :enable
+end
+
+package 'mysql-server' do
+  action :install
+end
+
+package 'php5-fpm' do
+  action :install
+end
+
+package 'php5-mysql' do
+  action :install
+end
